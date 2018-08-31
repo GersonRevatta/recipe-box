af258272c300e6bf9f1cdb291d4a2d6e3f4531c1
gerson <gersonrevatta@gmail.com>
2018-08-31 14:04:52 -0500

    Add haml and styles

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.3.7'
 gem 'jquery-rails'
 gem "paperclip", "~> 6.0.0"
+gem 'haml'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
   before_action :find_recipe, only: [:show, :edit, :update, :destroy]
   def index
-    @recipes = Recipe.all.order('created_at DESC')
+    @recipe = Recipe.all.order('created_at DESC')
   end
 
   def new
@@ -38,7 +38,7 @@ class RecipesController < ApplicationController
 
   private
   def recipe_params
-    params.require(:recipe).permit(:title, :description)
+    params.require(:recipe).permit(:title, :description, :image)
   end
 
   def find_recipe
